bd577436231d7acfcb53089216877448c46e988b
Jeongjihun <wlgns2234@naver.com>
2022-07-18 00:06:52 +0900

    views.py 경로 4개페이지 구성

--- Ealing_project/my_app/views.py
+++ Ealing_project/my_app/views.py
@@ -1,3 +1,15 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+def mainpage(request):
+    return render(request, 'my_app/mainpage.html')
+
+def page1(request): 
+    return render(request, 'my_app/page1.html')
+
+def page2(request):
+    return render(request, 'my_app/page2.html')
+
+def page3(request):
+    return render(request, 'my_app/page3.html')
