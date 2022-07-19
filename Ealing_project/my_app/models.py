43cd20eea10fd05069277811867717bf2a72c782
Jeongjihun <wlgns2234@naver.com>
2022-07-19 23:04:17 +0900

    model house coment

--- Ealing_project/my_app/models.py
+++ Ealing_project/my_app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class house(models.Model):
-    profile_img = models.ImageField(null=True)
-    name = models.CharField(max_length=20)
-    content = models.CharField(max_length=40)
-    price = models.CharField(max_length=20)
-    reputation = models.FloatField(max_length=20)
+    profile_img = models.ImageField(null=True) # 숙박이미지
+    name = models.CharField(max_length=20) # 지역 이름
+    content = models.CharField(max_length=40) # 힐링지 설명
+    price = models.CharField(max_length=20) # 가격
+    reputation = models.FloatField(max_length=20) # 평점
 
 	#uploadedFile = models.FileField(upload_to="")
--- Ealing_project/my_app/views.py
+++ Ealing_project/my_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from models import Item
 
 # Create your views here.
 def mainpage(request):
+    '''
+    if request.method == 'GET':
+        products = Item.objects.all()
+        context = {'products' : products}    
+        return render(request, 'my_app/mainpage.html', context)
+    '''
     return render(request, 'my_app/mainpage.html')
+    
 
 def page1(request): 
     return render(request, 'my_app/page1.html')
