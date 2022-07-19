b8ec8c5e23d6e2ad9fdcc32f6f7bcd6d0d94a081
Jeongjihun <wlgns2234@naver.com>
2022-07-20 06:12:31 +0900

    지역상세정보DB연동

--- Ealing_project/my_app/views.py
+++ Ealing_project/my_app/views.py
@@ -1,34 +1,30 @@
 from django.shortcuts import render
 from . import models
+from my_app.models import region_detail_page
 
 # 홈
 def mainpage(request):  
     if request.method == 'GET':
         products = models.house.objects.all()
-
         context = {
             'region1' : products[:5],
             'region2' : products[5:10],
             'recommendation1' : products[10:15],
             'recommendation2' : products[15:20]
         }
-
         return render(request, 'my_app/mainpage.html', context = context)
     
     #return render(request, 'my_app/mainpage.html')
     
 
-
 #체험
 def experience(request):
     if request.method == 'GET':
         products = models.house.objects.all()
-
         context = {
             'recommendation1' : products[10:15],
             'recommendation2' : products[15:20]
         }
-
         return render(request, 'my_app/experience.html', context = context)
 
 
@@ -36,16 +32,13 @@ def experience(request):
 def spotmap(request):
     if request.method == 'GET':
         place = models.recommend_place.objects.all()
-
         context = {'place' : place}
-
         return render(request, 'my_app/spotmap.html', context = context)
 
 
 
 def detail(request, topic):
     if request.method == 'GET':
-        #place = models.recommend_place.objects.all()
-    
-        context = {'topic' : topic}
-        return render(request, 'my_app/detail.html',context = context )
+            detail = models.region_detail_page.objects.all()
+            context = {'detail' : detail, 'topic' : topic}
+            return render(request, 'my_app/detail.html',context = context )
