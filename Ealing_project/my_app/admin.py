59bc34c527dde55540eeac046a22c276ede082f6
Jeongjihun <wlgns2234@naver.com>
2022-07-20 05:46:03 +0900

    region_detail_page model 세팅

--- Ealing_project/my_app/admin.py
+++ Ealing_project/my_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from my_app.models import house, recommend_place
+from my_app.models import house, recommend_place, region_detail_page
 
 
 # Register your models here.
 
 admin.site.register(house)
 admin.site.register(recommend_place)
+admin.site.register(region_detail_page)
--- Ealing_project/my_app/models.py
+++ Ealing_project/my_app/models.py
@@ -8,8 +8,6 @@ class house(models.Model):
     price = models.CharField(max_length=20) # 가격
     reputation = models.FloatField(max_length=20) # 평점
 
-	#uploadedFile = models.FileField(upload_to="")
-
     def __str__(self):
         return f"{self.name} - {self.content}"
 
@@ -38,9 +36,30 @@ class recommend_place(models.Model):
 
     recommend_load_img = models.TextField() # 추천코스이미지주소
 
-    
-
-	#uploadedFile = models.FileField(upload_to="")
-
     def __str__(self):
         return f"{self.name}"
+
+
+
+class region_detail_page(models.Model):
+    
+    img1 = models.TextField() # 작은이미지 주소
+    img2 = models.TextField() # 작은이미지 주소
+    img3 = models.TextField() # 작은이미지 주소
+    img4 = models.TextField() # 작은이미지 주소
+    img_big = models.TextField() # 작은이미지 주소
+    
+    profile_img = models.TextField() # 프로필이미지 주소
+
+    region_name = models.CharField(max_length=20) # 지역 이름(ex 전라남도 장성)
+    price = models.CharField(max_length=20) # 가격
+
+    title = models.CharField(max_length=30) 
+    content = models.CharField(max_length=500) 
+    small_title1 = models.CharField(max_length=30) 
+    small_content1 = models.CharField(max_length=500) 
+    small_title2 = models.CharField(max_length=30) 
+    small_content2 = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.region_name} 상세페이지"
