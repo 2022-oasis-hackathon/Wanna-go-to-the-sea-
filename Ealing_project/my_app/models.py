e05f2021adb3fb86979272ff9a0291ab439dda48
Jeongjihun <wlgns2234@naver.com>
2022-07-19 22:36:23 +0900

    add css, create house model

--- Ealing_project/my_app/models.py
+++ Ealing_project/my_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class house(models.Model):
+    name = models.CharField(max_length=20)
+    content = models.CharField(max_length=40)
+    price = models.CharField(max_length=20)
+    reputation = models.FloatField(max_length=20)
+
+	#uploadedFile = models.FileField(upload_to="")
