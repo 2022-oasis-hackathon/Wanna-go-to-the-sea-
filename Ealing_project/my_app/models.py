dd685e21c89c9d1562937a1148136788c356b0b4
Jeongjihun <wlgns2234@naver.com>
2022-07-19 22:49:07 +0900

    remodeling house model

--- Ealing_project/my_app/models.py
+++ Ealing_project/my_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class house(models.Model):
+    profile_img = models.ImageField(null=True)
     name = models.CharField(max_length=20)
     content = models.CharField(max_length=40)
     price = models.CharField(max_length=20)
